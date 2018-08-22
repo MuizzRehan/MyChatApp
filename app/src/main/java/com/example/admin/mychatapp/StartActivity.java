6677d10bd612b631aba5df33fdc1d391b2af2a59
Muizz Rehan <f148137@nu.edu.pk>
2018-08-23 03:27:46 +0500

    Registration Activity Start Activity

--- app/src/main/java/com/example/admin/mychatapp/StartActivity.java
+++ app/src/main/java/com/example/admin/mychatapp/StartActivity.java
@@ -1,13 +1,30 @@
 package com.example.admin.mychatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+    Button _registration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        _registration = findViewById(R.id.start_reg_button);
+
+        _registration.setOnClickListener(registration);
+
     }
+
+    public View.OnClickListener registration = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent reg_intent = new Intent(StartActivity.this, RegisterActivity.class);
+            startActivity(reg_intent);
+        }
+    };
 }
