08cac401cbd997b65ca2212df90c11b3a0f21d5e
Muizz Rehan <f148137@nu.edu.pk>
2018-08-23 05:12:02 +0500

    adding Mian_XML Toolbar

--- app/src/main/java/com/example/admin/mychatapp/MainActivity.java
+++ app/src/main/java/com/example/admin/mychatapp/MainActivity.java
@@ -3,20 +3,31 @@ package com.example.admin.mychatapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initializing Firebase Instance
         mAuth = FirebaseAuth.getInstance();
+
+        //Initializing Toolbar
+        toolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("MyChat App");
+
     }
+
 
     @Override
     public void onStart() {
