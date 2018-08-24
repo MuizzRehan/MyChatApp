e6605159f470d3d62dc08fda749ed67157c4629a
Muizz Rehan <f148137@nu.edu.pk>
2018-08-24 18:32:18 +0500

    Improving Designing Features

--- app/src/main/java/com/example/admin/mychatapp/LoginActivity.java
+++ app/src/main/java/com/example/admin/mychatapp/LoginActivity.java
@@ -71,6 +71,7 @@ public class LoginActivity extends AppCompatActivity {
                 if(task.isSuccessful()){
                     progressDialog.dismiss();
                     Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(mainIntent);
                 } else {
                     progressDialog.hide();
