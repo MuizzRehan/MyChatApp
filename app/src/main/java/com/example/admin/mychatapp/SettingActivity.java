fc20afdc2ae02ba755f4e47a9951f2a7ac6724d1
Muizz Rehan <f148137@nu.edu.pk>
2018-08-25 16:25:34 +0500

    Adding Image to Firebase Database Storage Download User Image and set it to imageView

--- app/src/main/java/com/example/admin/mychatapp/SettingActivity.java
+++ app/src/main/java/com/example/admin/mychatapp/SettingActivity.java
@@ -1,12 +1,19 @@
 package com.example.admin.mychatapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -14,31 +21,44 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingActivity extends AppCompatActivity {
     private DatabaseReference databaseReference;
     private FirebaseUser firebaseUser;
-
+    private StorageReference image_storageReference;
+    String current_uid;
     CircleImageView circleImageView;
     TextView user_name;
     TextView user_status;
     Button changeStatusBtn;
+    Button changeImage_btn;
+    ProgressDialog progressDialog;
+    int Gallery_Pick = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        image_storageReference = FirebaseStorage.getInstance().getReference();
         circleImageView = findViewById(R.id.setting_image);
         user_name = findViewById(R.id.setting_name);
         user_status = findViewById(R.id.setting_status);
         changeStatusBtn = findViewById(R.id.setting_changeStatus);
+        changeImage_btn = findViewById(R.id.setting_changeImg);
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        String current_uid = firebaseUser.getUid();
+        current_uid = firebaseUser.getUid();
 
         changeStatusBtn.setOnClickListener(changeStatus);
+        changeImage_btn.setOnClickListener(changeImage);
 
         databaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
         databaseReference.addValueEventListener(new ValueEventListener() {
@@ -51,6 +71,7 @@ public class SettingActivity extends AppCompatActivity {
 
                 user_name.setText(name);
                 user_status.setText(status);
+                Picasso.get().load(image).into(circleImageView);
             }
 
             @Override
@@ -68,4 +89,61 @@ public class SettingActivity extends AppCompatActivity {
             startActivity(statusActivity);
         }
     };
+
+    public View.OnClickListener changeImage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent galleryIntent = new Intent();
+            galleryIntent.setType("image/*");
+            galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(galleryIntent, Gallery_Pick);
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == Gallery_Pick && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                progressDialog = new ProgressDialog(SettingActivity.this);
+                progressDialog.setTitle("Uploading Image");
+                progressDialog.setMessage("Please wait while we upload the image.");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
+
+                Uri resultUri = result.getUri();
+                StorageReference filePath = image_storageReference.child("profile_images").child(current_uid + ".jpg");
+
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            String download_url = task.getResult().getDownloadUrl().toString();
+                            databaseReference.child("image").setValue(download_url).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(SettingActivity.this, "Successful Uploading", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        } else{
+                            Toast.makeText(SettingActivity.this, "not working", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
 }
